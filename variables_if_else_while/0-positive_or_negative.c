1cf9b2735eecefa8a68e39aa6cbc39a4e77ad499
jegomezV <jegomezvalencia@gmail.com>
2022-10-11 08:52:33 -0500

    random number

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/*main - punto de entrada
+Return: valor de regreso
+*/
+
+int main(void){
+
+int n;
+n = rand() - RAND_MAX / 2;
+
+if (n < 0 = es negativo);
+if (n > 0 = es positivo);
+if (n = 0 = es cero);
+
+
+return (0);
+}
