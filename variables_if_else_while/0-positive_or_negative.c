847d6c8d2a1812d08692a9e8b098cc64cfd0e702
jegomezV <jegomezvalencia@gmail.com>
2022-10-11 10:13:33 -0500

    random8

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ int main(void)
 
 	if (n < 0)
 	{
-		printf("%i is negative", n);
+		printf("%i is negative\n", n);
 	}
 		if (n > 0)
 	{
-		printf("%i is positive", n);
+		printf("%i is positive\n", n);
 	}
 		if (n == 0)
 	{
-		printf("%i is zero", n);
+		printf("%i is zero\n", n);
 	}
 
 	return (0);
