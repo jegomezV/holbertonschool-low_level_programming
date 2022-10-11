2de5064eb1bcd1c96dbedabc5d16c57181d6848a
jegomezV <jegomezvalencia@gmail.com>
2022-10-11 16:07:58 -0700

    helloW

---
+++ hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - punto de entrada
+ *
+ * Return: siempre 0
+*/
+
+int main(void)
+
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+
+}
