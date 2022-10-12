4edcd2c4f060adf8cd456b92bd231cbe2a61844c
jegomezV <jegomezvalencia@gmail.com>
2022-10-11 19:59:34 -0500

    -q-e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,18 @@
 #include<stdio.h>
+/**
+*main - entry point
+*Return:0
+*/
+int main(void)
+{
+	char alpha = 'a';
+
+		for (; alpha < 'z' ; alpha++)
+		{
+			if (alpha != 'q' || alpha != 'e')
+			putchar(alpha);
+		}
+		putchar('\n');
+
+		return (0);
+}
