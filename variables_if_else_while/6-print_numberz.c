86711cd3b5de35f3c5d2a3d45b14985a67f6fdd9
jegomezV <jegomezvalencia@gmail.com>
2022-10-11 22:20:05 -0500

    base 10-2

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,17 @@
 #include<stdio.h>
+/**
+*main - entry point
+*Return:0
+*/
+	int main(void)
+	{
+	int number = 0;
+
+	while (number < 10)
+{
+	number++;
+	putchar('0' + number);
+}
+	putchar('\n');
+	return (0);
+}
