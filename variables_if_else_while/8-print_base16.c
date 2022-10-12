13256bbe7104a890a7151050ab24e2db96b7d901
jegomezV <jegomezvalencia@gmail.com>
2022-10-12 09:37:30 -0500

    0-9-a-f

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,1 +1,21 @@
 #include<stdio.h>
+
+/**
+*main - entry point
+*Return: 0
+*/
+int main(void)
+{
+	char a = 'a';
+	int n = 0;
+
+		for (; n <= 9 ; n++)
+			putchar(0);
+
+		for (; a <= 'f' ; a++)
+			putchar(a);
+
+			putchar('\n');
+
+return (0);
+}
