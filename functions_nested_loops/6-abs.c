c40c7931f6de682cc14a886ec691656fbac83966
jegomezV <jegomezvalencia@gmail.com>
2022-10-13 15:58:30 -0500

    all

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - entry point
+ * @n:number
+ * Return: 0
+*/
+int _abs(int n)
+{
+	if (n < 0)
+    {
+        n = (-1) * n;
+        return (n);
+    }
+	else
+		return (0);
+}
