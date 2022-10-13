45aaa2634a18539f7328c3128fa4f60bf2d302cb
jegomezV <jegomezvalencia@gmail.com>
2022-10-12 19:52:20 -0500

    a-z-void

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - alpha
+*/
+void print_alphabet(void)
+{
+		char L;
+
+		for (L = 'a' ; L <= 'z' ; L++)
+		{
+			_putchar(L);
+		}
+
+		_putchar('\n');
+}
