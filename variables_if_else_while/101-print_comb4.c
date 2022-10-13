ebf72a8f9eda0e77eb1b4590ce86c8088d042883
jegomezV <jegomezvalencia@gmail.com>
2022-10-13 12:08:43 -0500

    no repeat3

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -2,28 +2,27 @@
 /**
  * main - prints all posibilities
  * Return: is 0
-*/
+ */
 int main(void)
 {
-    int b, n, m;
+	int b, n, m;
 
-    for ( b = '0' ; b <= '9'; b++)
-    {
-        for (n - b + 1 ; n <- '9'; n++)
-        {
-            for (m - n + 1 ; m <- '9' ; m++)
-            {
-                putchar(b);
-                putchar(n);
-                putchar(m);
-                if (b < '7')
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('/n')
-    return (0);
+	for (b = '0'; b <= '9'; b++)
+	{
+		for (n - b + 1; n < -'9'; n++)
+		{
+			for (m - n + 1; m < -'9'; m++)
+			{
+				putchar(b);
+				putchar(n);
+				putchar(m);
+				if (b < '7')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('/n') return (0);
 }
