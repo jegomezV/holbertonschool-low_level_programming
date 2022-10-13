23822ef3b8b461a1b9fb2f4a7f42647350b8ddf7
jegomezV <jegomezvalencia@gmail.com>
2022-10-12 20:53:34 -0500

    A-Z-10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - 10xalpha
+*/
+
+void print_alphabet_x10(void)
+{
+		int n = 0;
+		char L;
+
+		while (1 <= 10)
+	{
+		for (L = 'a' ; L <= 'z' ; L++)
+		{
+			_putchar(L);
+		}
+		n++;
+
+		_putchar('\n');
+	}
+
+
+}
