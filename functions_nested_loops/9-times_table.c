fd2f838582deec0b33fc36c4cb908a3b10f303e3
jegomezV <jegomezvalencia@gmail.com>
2022-10-14 08:56:12 -0500

    x92

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,40 +5,40 @@
 
 void times_table(void)
 {
-    int filas;
-    int colum;
-    int c;
+	int filas;
+	int colum;
+	int c;
 
-    c = 0;
+	c = 0;
 
-        for (filas = 0; filas <= 9; filas++)
-        {
-            for (colum = 0; colum <= 9; colum++)
-            {
-                int operation;
+	for (filas = 0; filas <= 9; filas++)
+	{
+		for (colum = 0; colum <= 9; colum++)
+		{
+			int operation;
 
-                operation = c * colum;
+			operation = c * colum;
 
-                if (operation > 9)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar((operation / 10) + '0');
-                    _putchar((operation % 10) + '0');
-                }
-                else if (colum == 0)
-                {
-                    _putchar(operation + '0');
-                }
-                else
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(operation + '0');
-                }
-            }
-            c++;
-            _putchar('\n');
-        }
+			if (operation > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((operation / 10) + '0');
+				_putchar((operation % 10) + '0');
+			}
+			else if (colum == 0)
+			{
+				_putchar(operation + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(operation + '0');
+			}
+		}
+		c++;
+		_putchar('\n');
+	}
 }
