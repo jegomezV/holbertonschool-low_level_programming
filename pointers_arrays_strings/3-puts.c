4ca2f81efe2772ce64025cab99e345c6556faf24
jegomezV <jegomezvalencia@gmail.com>
2022-10-17 17:58:16 -0500

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - print
+ * @str: string
+*/
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
