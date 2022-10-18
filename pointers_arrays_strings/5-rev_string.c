12e7d3e294e04f2c8d5e7545c9b7a56a8a796b25
jegomezV <jegomezvalencia@gmail.com>
2022-10-18 03:03:07 -0500

    reverse

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,31 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
+#include <conio.h>
 /**
  * rev_string - reverse string
  *
  */
 void rev_string(char *s)
 {
-
-	char *texto;
-	int posicionfinal;
-
-	if (posicionfinal < 0)
-	{
-		return;
-	}
-	else
-	{
-		int aux = *(texto);
-		*(texto) = *(texto + posicionfinal);
-		*(texto = posicionfinal) = aux;
-		invertirtexto(texto + 1, posicionfinal - 2);
-	}
-
-	char *s;
-	invertirtexto(texto, 9);
-	putchar(s + '0');
+char s[10] = "My School";
+strrev(s);
+puts(s);
 
 	return (0);
 }
