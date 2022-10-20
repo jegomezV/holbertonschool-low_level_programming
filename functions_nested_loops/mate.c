76c63347f085c17afc28464dd89c38d1de627f2e
jegomezV <jegomezvalencia@gmail.com>
2022-10-20 16:41:51 -0500

    conc

---
+++ functions_nested_loops/mate.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    int h = 1, a = 0;
+    int b = 10;
+    
+
+    printf("resultado %d/n", h / b);
+
+    return (0),
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -13,3 +13,4 @@ char *_strcpy(char *dest, char *src);
 int _putchar(char c);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
