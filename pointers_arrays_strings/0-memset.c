5ab72152d7d8c65ee9899f2b555d1b1da51b38db
jegomezV <jegomezvalencia@gmail.com>
2022-10-21 17:16:41 -0500

    .

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,11 +1,16 @@
 #include "main.h"
-
+/**
+ * _memset - memori space
+ * @s:pointer
+ * @b:constant
+ * @n:bytes
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
 
 	unsigned int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
