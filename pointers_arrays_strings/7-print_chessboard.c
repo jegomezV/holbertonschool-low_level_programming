b978013ff615ee7fddc19d35b4dd2a59778de3fc
jegomezV <jegomezvalencia@gmail.com>
2022-10-24 20:52:32 -0500

    .

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chessboard - code
+ *
+ * @a: array
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+
+	int c, f;
+
+	for (c = 0; c < 8; c++)
+	{
+		for (f = 0; f < 8; f++)
+		{
+			putchar(a[c][f]);
+		}
+		putchar('\n');
+	}
+}
