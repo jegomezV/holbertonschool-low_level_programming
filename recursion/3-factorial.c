16d8a7aeda54c020f5578ecdfebba2f044d8315d
jegomezV <jegomezvalencia@gmail.com>
2022-10-24 21:18:17 -0500

    .

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,8 +1,17 @@
 #include "main.h"
 
-int _pow_recursion(int x, int y)
+/**
+ *factorial - function of a given number
+ *@n: numbers
+ *Return:pointers
+ */
+int factorial(int n)
 {
+	if (n < 0)
+		return (-1);
 
+	if (n == 0)
+		return (1);
 
-    
+	return (n * factorial(n - 1));
 }
