b84a1ae102b23d75c5dc6fa3569d255e13c15394
jegomezV <jegomezvalencia@gmail.com>
2022-10-27 18:06:20 -0500

    argc

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *main- Entry point
+ *@argc: argument counter
+ *@argv: vector
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+   * root - if a number is prime
+   *
+   * @n: the number
+   * @i: the numbers we'll go through
+   *
+   * Return: Whether or not the number is a prime number
+   */
+
+int root(int n, int i)
+{
+	if (n <= 1 || n % i == 0)
+		return (0);
+	else if (n == i)
+		return (1);
+	else if (n > i)
+		root(n, i + 1);
+
+	return (1);
+}
+
+/**
+   * is_prime_number - tells us if an integer is a prime number or not
+   *
+   * @n: the number to check
+   *
+   * Return: 0 if the number is not prime, and 1 if it is
+   */
+
+int is_prime_number(int n)
+{
+	return (root(n, 2));
+}
