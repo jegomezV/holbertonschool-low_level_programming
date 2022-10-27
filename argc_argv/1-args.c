d78e1400326123ae6a51cdb485e6234eceda167f
jegomezV <jegomezvalencia@gmail.com>
2022-10-27 18:09:24 -0500

    argc3

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%i\n", argc - 1);
+	(void)argv;
+	return (0);
+}
