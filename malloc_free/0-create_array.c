a86752a27ef4b4c384dd2f972f2eca3c7bcd15d4
jegomezV <jegomezvalencia@gmail.com>
2022-10-31 17:46:38 -0500

    .

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_array - function name
+ * @size: size
+ * @c: character 
+ * Return: NULL - arr
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int x;
+	char *arr;
+
+	if (size == 0)
+		return (NULL);
+
+	arr = malloc(sizeof(char) * size);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (x = 0; x < size; x++)
+		arr[x] = c;
+
+	return (arr);
+}
