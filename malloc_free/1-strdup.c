8443adcdf921b14c43a5db14bdd072d144ed5e6d
jegomezV <jegomezvalencia@gmail.com>
2022-11-01 09:05:17 -0500

    duplicated

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -13,14 +13,14 @@ char *_strdup(char *str)
 		return (NULL);
 
 	for (a = 0; str[a] != '\0'; a++)
-
+		{}
 		ptr = malloc(sizeof(char) * (a));
 
 	if (ptr == NULL)
 		return (NULL);
 
 	for (b = 0; str[b] != '\0'; b++)
-
+	{}
 		ptr[b] = str[b];
 
 	return (ptr);
---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+char *str_concat(char *s1, char *s2)
+{
+    
+}
