1d3e9fa1e6153793aff81e40c7221d6c0c4f040c
jegomezV <jegomezvalencia@gmail.com>
2022-11-01 12:12:54 -0500

    concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,6 +1,39 @@
 #include "main.h"
-
+/**
+ * *str_concat - function
+ * @s1: string 1
+ * @s2: string 2
+ * Return: ptr
+*/
 char *str_concat(char *s1, char *s2)
 {
-    
+	int a, b, c, d;
+	char *ptr;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	for (a = 0; s1[a] != '\0'; a++)
+	{
+	}
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (b = 0; s2[b] != '\0'; b++)
+	{
+	}
+
+	ptr = malloc(sizeof(char) * a + b + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (c = 0; s1[c] != '\0'; c++)
+		ptr[c] = s1[c];
+
+	for (d = 0; s2[d] != '\0'; d++)
+		ptr[d + a] = s2[d];
+
+	return (ptr);
 }
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
