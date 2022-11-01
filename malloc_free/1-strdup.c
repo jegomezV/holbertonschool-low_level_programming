c71764b293e3223585e4198a7c818a77e5521846
jegomezV <jegomezvalencia@gmail.com>
2022-11-01 08:35:08 -0500

    duplicated

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strdup - copy a string
+ * @str: string
+ * Return: a pointer
+*/
+char *_strdup(char *str)
+{
+	int a, b;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+
+				   for (a = 0; str[a] != '\0'; a++)
+
+					   ptr = malloc(sizeof(char) * (a + 1));
+	if (ptr == NULL)
+		return (NULL);
+
+				   for (b = 0; str[b] != '\0'; b++)
+
+					   ptr[b] = str[b];
+
+	return (ptr);
+}
