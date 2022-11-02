7ed4bdd38a06cc10bbda8dbdda5fe70a84d10fb9
jegomezV <jegomezvalencia@gmail.com>
2022-11-02 17:05:50 -0500

    malloc check

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * *malloc_checked - a function name
+ * @b: a number
+ * Return: the pointer
+*/
+void *malloc_checked(unsigned int b)
+{
+int *ptr = 0;
+
+ptr = malloc(b);
+if (b == NULL)
+	exit(98);
+
+return (b);
+}
