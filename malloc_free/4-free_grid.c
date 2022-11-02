54037fbcb75f4918edb5a7ba2d1870cc61cd3e11
jegomezV <jegomezvalencia@gmail.com>
2022-11-01 19:00:56 -0500

    free

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -3,14 +3,14 @@
  * free_grid - function
  * @height: length
  * @grid: a bidimensional pointer
-*/
+ */
 void free_grid(int **grid, int height)
 {
-    int a;
+	int a;
 
-    for (a = 0; a < height; a++)
-    {
-        free(grid[a]);
-    }
-    free(grid);
+	for (a = 0; a < height; a++)
+	{
+		free(grid[a]);
+	}
+	free(grid);
 }
