e37c19135c69af73868c63ebf7dbb4b5451c6073
jegomezV <jegomezvalencia@gmail.com>
2022-11-01 19:00:07 -0500

    free

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * free_grid - function
+ * @height: length
+ * @grid: a bidimensional pointer
+*/
+void free_grid(int **grid, int height)
+{
+    int a;
+
+    for (a = 0; a < height; a++)
+    {
+        free(grid[a]);
+    }
+    free(grid);
+}
