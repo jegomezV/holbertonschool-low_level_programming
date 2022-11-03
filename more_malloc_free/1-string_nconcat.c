e21943df13bb1eaf46107f4b2e846f42e75cd6eb
jegomezV <jegomezvalencia@gmail.com>
2022-11-03 10:51:38 -0500

    concatenate 2

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * *string_nconcat - a function to concatenate
+ * @s1:string 1
+ * @s2:string 2
+ * @n:string 3
+ * Return:  pointer or NULL
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int s1l, s2l, p, c = 0, 0, 0, 0;
+	char *ptr = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (; s1[s1l] != '\0'; s1l++)
+
+		for (; s2[s2l] != '\0'; s2l++)
+
+			if (n > s2l)
+				ptr = malloc(sizeof(char) * s1l + s2l + 1);
+			else
+				ptr = malloc(sizeof(char) * s1 + n + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (; p < s1l; p++)
+		ptr[p] = s1[p];
+
+	for (c = 0; c < n && c < s2l; c++, p++)
+		ptr[p] = s2[c];
+
+	return (ptr);
+}
