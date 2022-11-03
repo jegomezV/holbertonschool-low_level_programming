cb8fd44a5441b0fb8594b8035dda80fd6e1c2c76
jegomezV <jegomezvalencia@gmail.com>
2022-11-03 13:01:26 -0500

    concat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -8,42 +8,36 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l, a, p, c;
-	l = 0;
-	a = 0;
-	p = 0;
-	c = 0;
-	char *ptr = 0;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
-	for (l = 0; s1[l] != '\0'; l++)
-
-	for (a = 0; s2[a] != '\0'; a++)
-
-	if (n > a)
-	{
-		ptr = malloc((l + 1 + a) * sizeof(char));
-	}
+	while (s1[i])
+		i++;
+	while (s2[k])
+		k++;
+	if (n >= k)
+		l = i + k;
 	else
-	{
-		ptr = malloc((l + 1 + n) * sizeof(char));
-	}
-
-	if (ptr == NULL)
+		l = i + n;
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
 		return (NULL);
-
-	for (p = 0; p < l; p++)
-		ptr[p] = s1[p];
-
-	for (c = 0; c < n && c < a; c++, p++)
-		ptr[p] = s2[c];
-
-	ptr[p] = '\0';
-
-	return (ptr);
+	k = 0;
+	while (j < l)
+	{
+		if (j <= i)
+			str[j] = s1[j];
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
 }
