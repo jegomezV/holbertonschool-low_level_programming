f0bf754cf1f6cc051caf6b020f854b5eb43a53fb
jegomezV <jegomezvalencia@gmail.com>
2022-11-07 19:25:30 -0500

    struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,23 +2,7 @@
 
 struct dog
 {
-char *name;
+char * name;
 float age;
-char *owner;
+char * owner;
 };
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    struct dog my_dog;
-
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
-}
