900ecec024f87df69b88771421c2607133a32ba4
jegomezV <jegomezvalencia@gmail.com>
2022-11-09 11:35:06 -0500

    print_name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,7 +1,12 @@
 #include "function_pointers.h"
-
+/**
+ * print_name - function name
+ * @f: the pointer
+ * @name: the name
+ * Return: void
+*/
 void print_name(char *name, void (*f)(char *))
 {
     if (f != NULL)
-        f = (name);
+        (f) = (name);
 }
