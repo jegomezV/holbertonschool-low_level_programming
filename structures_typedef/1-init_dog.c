7a9f2d9eb5bf5e04b070152b057bdd3c0a86b752
jegomezV <jegomezvalencia@gmail.com>
2022-11-09 17:07:44 -0500

    init dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * init_dog - the function name
+ * @d:the pointer to do the operation
+ * @name:the pointer with the name
+ * @age:the pointer with the dog age
+ * @owner:the pointer with the owner name
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+		(*d).name = name;
+		d -> age = age;
+		(*d).owner = owner;
+
+}
---
+++ structures_typedef/main.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
