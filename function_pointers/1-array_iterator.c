9177db7a1859ac9dfc7cc1bccc39b967473315d4
jegomezV <jegomezvalencia@gmail.com>
2022-11-10 16:54:53 -0500

    array_itterator

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -5,15 +5,14 @@
  * @size:array dimensions
  * @action:the pointer to the function
  * Return:void
-*/
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    int size1 = size;
-    int length;
+	int size1 = size;
+	int length;
 
-    if (array && action)
+	if (array && action)
 
-    for (length = 0; length < size1; length++)
-        action(array[length]);
-
+	for (length = 0; length < size1; length++)
+		action(array[length]);
 }
