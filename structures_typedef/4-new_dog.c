b53ac5ddd52cd20631dd1794ec41829deae4e4cd
jegomezV <jegomezvalencia@gmail.com>
2022-11-10 07:47:26 -0500

    free_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -36,8 +36,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		new_dog->owner = malloc(ownerl + 1);
 		if (owner == NULL)
 			{
-			free(new_dog->name);
 			free(new_dog->owner);
+			free(new_dog->name);
 			free(new_dog);
 			return (NULL);
 			}
