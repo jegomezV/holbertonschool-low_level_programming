bc1e55fab06a8f4733c2de59e44938498d4d5232
jegomezV <jegomezvalencia@gmail.com>
2022-11-11 20:46:49 -0500

    calc

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -5,24 +5,22 @@
  * Return: right op, NULL if not
  */
 
-int (*get_op_func(char *s))(int, int){
+int (*get_op_func(char *s))(int, int)
+{
 
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
+	int i = 0;
 
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	{
+		i++;
+	}
 
-
-    while (ops[i].op != NULL && *(ops[i].op) != *s)
-    {
-        i++;
-    }
-
-    return (ops[i].f);
+	return (ops[i].f);
 }
