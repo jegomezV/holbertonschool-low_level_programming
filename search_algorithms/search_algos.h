ee0281f54e5cbc6cee507d0a6c438b2a4457304c
jegomezV <jegomezvalencia@gmail.com>
2023-01-13 23:27:53 -0500

    proyect

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,12 +1,14 @@
-#ifndef _SEARCH_ALGOS_H_
-#define _SEARCH_ALGOS_H_
+#ifndef HEADER_FILE_H
+#define HEADER_FILE_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
